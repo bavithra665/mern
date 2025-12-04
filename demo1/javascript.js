c68d8e8ca139ec0de4dba5ccb884a411a48fa66b
Bavithra <bavithra.r2024cse@sece.ac.in>
2025-12-04 10:21:37 +0530

    updated assignments

---
+++ demo1/javascript.js
@@ -0,0 +1,15 @@
+var a=5; //var
+var a=10;
+a=15;
+console.log(a);
+
+// let
+let a = 5;
+// let a = 10; ❌ not allowed
+a = 10;     // ✔ allowed
+console.log(a);
+
+// const can be redeclared or update
+const pi=3.14;
+console.log(pi);
+
